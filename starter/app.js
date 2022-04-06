0a5c2cbf6dd5f231c1e75b7184c5204b46ff1160
tomsantos07 <tomsantos07>
2022-04-06 10:31:44 -0300

    create the middleware, requiring the tasks

--- starter/app.js
+++ starter/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
+
+
+// middleware
+app.use(express.json()) // if we don't have it, there's no rec.body data
 
 // routes
 app.get('/hello', (req, res) => {
   res.send('Task Manager App')
 });
+
+app.use('/api/v1/tasks', tasks)
 
 const port = 3000;
 
