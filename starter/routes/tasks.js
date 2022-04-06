fd97a38882bf30c929044d73e6ceae71c80f1a79
tomsantos07 <tomsantos07>
2022-04-06 10:30:50 -0300

    get the the functions and pass to the routes

--- starter/routes/tasks.js
+++ starter/routes/tasks.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router();
 
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/tasks');
 
-router.route('/').get((req, res) => {
-  res.send('all items')
-});
+router.route('/').get(getAllTasks).post(createTask);
+// what's with that 'patch'???
+// https://medium.com/@gabrielrufino.js/put-vs-patch-pare-de-agora-escolher-errado-533b8c6058d9
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
